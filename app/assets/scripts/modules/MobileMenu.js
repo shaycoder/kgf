d672d17bf384af51e8ee2de92ea74281466788b9
Nitin Rajan <nitin.rajan15@gmail.com>
2023-08-25 11:36:00 +0530

    Modified Css and Javascript code

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -3,11 +3,23 @@ class MobileMenu {
         this.menuIcon = document.querySelector('.site-header__menu-icon')
         this.menuContent = document.querySelector('.site-header__menu-content')
         this.menuHeader = document.querySelector('.site-header')
+        this.menuElement = document.querySelectorAll('.menu-element, .wrapper')
         this.events()
     }
 
     events() {
         this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+        this.menuElement.forEach(el => {
+            el.addEventListener('click', () => {
+              this.removeTheMenu()
+            });
+        });
+    }
+
+    removeTheMenu() {
+        this.menuContent.classList.remove('site-header__menu-content--is-visible')
+        this.menuHeader.classList.remove('site-header--is-expanded')
+        this.menuIcon.classList.remove('site-header__menu-icon--close-x')
     }
 
     toggleTheMenu() {
@@ -15,6 +27,7 @@ class MobileMenu {
         this.menuHeader.classList.toggle('site-header--is-expanded')
         this.menuIcon.classList.toggle('site-header__menu-icon--close-x')
     }
+    
 }
 
 export default MobileMenu;
